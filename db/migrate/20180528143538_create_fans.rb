6752e69f04982c8d1626f953b9c7616530fcdf42
Victor Lacombe <lacombe.victor@hotmail.fr>
2018-05-28 17:25:12 +0200

    changed username to youtube_username

--- db/migrate/20180528143538_create_fans.rb
+++ db/migrate/20180528143538_create_fans.rb
@@ -1,7 +1,7 @@
 class CreateFans < ActiveRecord::Migration[5.2]
   def change
     create_table :fans do |t|
-      t.string :username
+      t.string :youtube_username
       t.string :channel_id_youtube
       t.string :profile_picture_url
       t.references :memo, foreign_key: true
