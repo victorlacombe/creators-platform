4d464b329d72da5a9ac7033f925a27829009c24a
Eliott GUERIN <eliott.guerin@gmail.com>
2018-05-29 11:40:13 +0200

    routes user and controller settings for google auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # the callback routes are defined for omniauth
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   get 'confidentiality', to: 'pages#confidentiality', as: :confidentiality
 
@@ -14,7 +15,4 @@ Rails.application.routes.draw do
 
   # COMMENTS : no need for a comments view, will be displayed in the fans view.
   resources :comments, only: [:create, :update]
-
-  # the callback routes are defined for omniauth
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
