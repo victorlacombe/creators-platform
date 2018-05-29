1cdcd1f402ab4bac4e369f56741e8ac145f403cc
Julien Chim <julien.chim@gmail.com>
2018-05-29 18:25:17 +0200

    en cours

--- app/services/youtube_refresh_data_service.rb
+++ app/services/youtube_refresh_data_service.rb
@@ -1,4 +1,4 @@
-# YoutubeRefreshDataService.new("UCcIZ5L8w-VXDim5r7sINIww")
+# YoutubeRefreshDataService.new(id: "UCcIZ5L8w-VXDim5r7sINIww")
 
 class YoutubeRefreshDataService
   def initialize(user)
@@ -6,65 +6,76 @@ class YoutubeRefreshDataService
     @yt_channel = Yt::Channel.new(id: @user.channel_id_youtube)
   end
 
-  def update_channel_data
-    @user.channel_name = yt_channel.title # yt makes an API Request. 'channel' is then populated with data.
-    @user.channel_thumbnail = yt_channel.thumbnail_url # Does yt make a request again? Data is a bit different
+  def refresh_channel_data
+    @user.channel_name = @yt_channel.title # yt makes an API Request. 'channel' is then populated with data.
+    @user.channel_thumbnail = @yt_channel.thumbnail_url # Does yt make a request again? Data is a bit different
     # @user.channel_comment_count = channel.comment_count # Doesn't seem to work.. Always return 0
+    @user.save
   end
 
-  def new_video_data
+  def refresh_all_videos
+    # Get all videos from Youtube
     @yt_channel.videos.each do |yt_video|
+      # Check if video exists in db
       if @user.videos.find_by(video_id_youtube: yt_video.id).nil?
+        # video is not existing, we create a new one
         db_video = Videos.new
-        db_video.title = yt_video.title
-        db_video.thumbnail = yt_video.thumbnail_url
-        db_video.likes = yt_video.like_count
-        db_video.dislikes = yt_video.dislike_count
-        db_video.comment_count = yt_video.comment_count
-        db_video.save
+        db_video = set_video_data(db_video, yt_video)
+      else
+        # video exists, we update the existing one
+        db_video = @user.videos.find_by(video_id_youtube: yt_video.id)
+        db_video = set_video_data(db_video, yt_video)
+      end
+      db_video.save
+    end
+  end
+
+  def refresh_all_comments(yt_video)
+    @yt_channel.videos.each do |yt_video|
+      # Check if video exists in db
+      if @user.videos.find_by(video_id_youtube: yt_video.id).nil?
+        # Video does not exist in db. We do nothing.
+      else
+        # video exists, we update the comments
+        if comment_count_changed?(yt_video, db_video)
+          # update data in db
+          @user.videos.comments
+          comments = yt_video.comment_threads # Get all parent comments of a video
+          comments.each do |comment|
+            comment.text_display # content
+            comment_thread.author_display_name # author
+            comment_thread.updated_at # published_at
+            comment_thread.top_level_comment # top_level_comment_id_youtube
+          end
+        else
+          # we do nothing. However we may miss a comment "edit" or if there is a add + delete of comments.
+        end
       end
     end
   end
 
-  # def update_video_data
-  #   # we find the related video in our db
-  #   db_video = @user.videos.find_by(video_id_youtube: yt_video.id)
-  #   db_video.title = yt_video.title
-  #   db_video.thumbnail = yt_video.thumbnail_url
-  #   db_video.likes = yt_video.like_count
-  #   db_video.dislikes = yt_video.dislike_count
-  # end
+  private
 
-  # def get_new_comments
-  #   @yt_channel.videos.each do |yt_video|
-  #     # we find the related video in our db
-  #     db_video = @user.videos.find_by(video_id_youtube: yt_video.id)
+  def set_video_data(db_video, yt_video)
+    db_video.title = yt_video.title
+    db_video.thumbnail = yt_video.thumbnail_url
+    db_video.likes = yt_video.like_count
+    db_video.dislikes = yt_video.dislike_count
+    db_video.comment_count = yt_video.comment_count
+    return db_video
+  end
 
-  #     # check if numbers of comments has changed
-  #     yt_comment_count = yt_video.comment_count
-  #     db_comment_count = db_video.comment_count
-  #     if yt_comment_count != db_comment_count
-  #       # update data in db
+  def comment_count_changed?(yt_video, db_video)
+    # check if numbers of comments has changed
+    yt_comment_count = yt_video.comment_count
+    db_comment_count = db_video.comment_count
+    yt_comment_count != db_comment_count ? true : false
+  end
 
-  #     end
-  #   end
-  # end
+  def set_comment_data()
+  end
 end
 
-# # # COMMENTS
-# # Get all parent comments of a video
-# video.comment_threads # => <Yt::Collections::CommentThreads ...>
-# # Get all parent comments of a channel
-# channel.comment_threads # => <Yt::Collections::CommentThreads ...>
-# # content
-# comment_thread.text_display
-# # author
-# comment_thread.author_display_name
-# # published_at
-# comment_thread.updated_at
-# # top_level_comment_id_youtube
-# comment_thread.top_level_comment
-
-# # # FANS
-# # channel_id_youtube
-# # profile_picture_url
+# # FANS
+# channel_id_youtube
+# profile_picture_url
---
+++ db/schema.rb
@@ -0,0 +1,93 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_05_29_135555) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.datetime "published_at"
+    t.boolean "is_pinned"
+    t.string "top_level_comment_id_youtube"
+    t.bigint "video_id"
+    t.bigint "fan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["fan_id"], name: "index_comments_on_fan_id"
+    t.index ["video_id"], name: "index_comments_on_video_id"
+  end
+
+  create_table "fans", force: :cascade do |t|
+    t.string "youtube_username"
+    t.string "channel_id_youtube"
+    t.string "profile_picture_url"
+    t.bigint "memo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["memo_id"], name: "index_fans_on_memo_id"
+  end
+
+  create_table "memos", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_memos_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "token"
+    t.datetime "token_expiry"
+    t.string "channel_name"
+    t.string "channel_thumbnail"
+    t.string "channel_id_youtube"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string "video_id_youtube"
+    t.string "title"
+    t.string "thumbnail"
+    t.integer "likes"
+    t.integer "dislikes"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "comment_count"
+    t.index ["user_id"], name: "index_videos_on_user_id"
+  end
+
+  add_foreign_key "comments", "fans"
+  add_foreign_key "comments", "videos"
+  add_foreign_key "fans", "memos"
+  add_foreign_key "memos", "users"
+  add_foreign_key "videos", "users"
+end
