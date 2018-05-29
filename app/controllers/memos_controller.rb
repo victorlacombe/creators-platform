d532c17681f1b997305425b487bc46d93b0b0f2c
Victor Lacombe <lacombe.victor@hotmail.fr>
2018-05-29 12:50:17 +0200

    created create edit and update method for memos

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,26 @@
 class MemosController < ApplicationController
+  def create
+    @memo = Memo.create
+    authorize @memo
+  end
+
+  def edit
+  # this method will have to be deleted since we will display the form directly
+  # inside of the fans view
+    @memo = Memo.find(params[:id])
+    authorize @memo
+  end
+
+  def update
+    @memo = Memo.find(params[:id])
+    authorize @memo
+    @memo.update(memo_params)
+    redirect_to edit_memo_path(@memo)
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:content)
+  end
 end
