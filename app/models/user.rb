9e0616ec3bb020574eeeabcf4312f81f185417c2
Eliott GUERIN <eliott.guerin@gmail.com>
2018-05-29 14:02:13 +0200

    user model connection to omniauth datas : last name add

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,10 @@ class User < ApplicationRecord
 
       # Uncomment the section below if you want users to be created if they don't exist
       unless user
-          user = User.create(name: data['name'],
-             email: data['email'],
-             password: Devise.friendly_token[0,20]
+          user = User.create(first_name: data['first_name'],
+            last_name: data['last_name'],
+            email: data['email'],
+            password: Devise.friendly_token[0,20]
           )
       end
       user
