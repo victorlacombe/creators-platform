19b390e485c225845abfa050222b129278d78cb6
Julien Chim <julien.chim@gmail.com>
2018-05-29 16:25:29 +0200

    adding a service

---
+++ app/services/youtube_refresh_data_service.rb
@@ -0,0 +1,70 @@
+# YoutubeRefreshDataService.new("UCcIZ5L8w-VXDim5r7sINIww")
+
+class YoutubeRefreshDataService
+  def initialize(channel_id_youtube)
+    @yt_channel = Yt::Channel.new(id: channel_id_youtube)
+    @user = current_user
+  end
+
+  def update_channel_data
+    @user.channel_name = yt_channel.title # yt makes an API Request. 'channel' is then populated with data.
+    @user.channel_thumbnail = yt_channel.thumbnail_url # Does yt make a request again? Data is a bit different
+    # @user.channel_comment_count = channel.comment_count # Doesn't seem to work.. Always return 0
+  end
+
+  def new_video_data
+    @yt_channel.videos.each do |yt_video|
+      if @user.videos.find_by(video_id_youtube: yt_video.id).nil?
+        db_video = Videos.new
+        db_video.title = yt_video.title
+        db_video.thumbnail = yt_video.thumbnail_url
+        db_video.likes = yt_video.like_count
+        db_video.dislikes = yt_video.dislike_count
+        db_video.comment_count = yt_video.comment_count
+        db_video.save
+      end
+    end
+  end
+
+  # def update_video_data
+  #   # we find the related video in our db
+  #   db_video = @user.videos.find_by(video_id_youtube: yt_video.id)
+  #   db_video.title = yt_video.title
+  #   db_video.thumbnail = yt_video.thumbnail_url
+  #   db_video.likes = yt_video.like_count
+  #   db_video.dislikes = yt_video.dislike_count
+  # end
+
+  # def get_new_comments
+  #   @yt_channel.videos.each do |yt_video|
+  #     # we find the related video in our db
+  #     db_video = @user.videos.find_by(video_id_youtube: yt_video.id)
+
+  #     # check if numbers of comments has changed
+  #     yt_comment_count = yt_video.comment_count
+  #     db_comment_count = db_video.comment_count
+  #     if yt_comment_count != db_comment_count
+  #       # update data in db
+
+  #     end
+  #   end
+  # end
+end
+
+# # # COMMENTS
+# # Get all parent comments of a video
+# video.comment_threads # => <Yt::Collections::CommentThreads ...>
+# # Get all parent comments of a channel
+# channel.comment_threads # => <Yt::Collections::CommentThreads ...>
+# # content
+# comment_thread.text_display
+# # author
+# comment_thread.author_display_name
+# # published_at
+# comment_thread.updated_at
+# # top_level_comment_id_youtube
+# comment_thread.top_level_comment
+
+# # # FANS
+# # channel_id_youtube
+# # profile_picture_url
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_143539) do
+ActiveRecord::Schema.define(version: 2018_05_29_133647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 2018_05_28_143539) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "channel_name"
+    t.string "channel_thumbnail"
+    t.integer "channel_comment_count"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -72,6 +75,7 @@ ActiveRecord::Schema.define(version: 2018_05_28_143539) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comment_count"
     t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
