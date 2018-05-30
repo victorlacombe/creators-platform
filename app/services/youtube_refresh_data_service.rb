292d606a54cc609eab5b2fc836ee191b16296ff1
Julien Chim <julien.chim@gmail.com>
2018-05-30 19:44:23 +0200

    correct url missing

--- app/services/youtube_refresh_data_service.rb
+++ app/services/youtube_refresh_data_service.rb
@@ -80,7 +80,7 @@ class YoutubeRefreshDataService
     # ------------------------
     channel_id = @user.channel_id_youtube
     max_results = 100 # Youtube API V3 limit comments list to 100 results - https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.commentThreads.list?part=snippet%252Creplies&maxResults=100&videoId=1cMoaxEPJ_Y&_h=18&
-    "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=#{max_results}&allThreadsRelatedToChannelId=#{channel_id}&key=#{@youtube_api_v3}"
+    url = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=#{max_results}&allThreadsRelatedToChannelId=#{channel_id}&key=#{@youtube_api_v3}"
     response = RestClient.get(url)
     h_response = JSON.parse(response)
 
