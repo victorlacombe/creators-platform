14d3223ddcba1e0ca1478acd0e0b965c4984d4f4
Victor Lacombe <lacombe.victor@hotmail.fr>
2018-05-30 10:18:35 +0200

    added popup to chrome extension

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,13 +1,29 @@
 console.log("Ready to go");
 
-chrome.runtime.onMessage.addListener(gotMessage);
 
-function gotMessage(message, sender, sendResponse) {
-  console.log(message)
-  if (message === "hello") {
-    const videoTitle = document.querySelectorAll("#video-title")
-    videoTitle.forEach(function() {
-      videoTitle.innerText = "COIN COIN !"
-    })
-  }
-}
+// chrome.runtime.onMessage.addListener(gotMessage);
+
+// function gotMessage(message, sender, sendResponse) {
+//   console.log(message)
+//   if (message === "hello") {
+//     const videoTitles = document.querySelectorAll("#video-title")
+//     videoTitles.forEach (function(videoTitle) {
+//       videoTitle.innerText = "COIN COIN !"
+//     })
+//   }
+// }
+
+const dashboardCommentAuthors = document.querySelectorAll(".comment-header");
+
+dashboardCommentAuthors.forEach(function(dashboardCommentAuthor) {
+  dashboardCommentAuthor.insertAdjacentHTML("beforeend", '<a href="https://www.youtube.com/comments">CLICK ME</a>' )
+})
+
+// document.addEventListener("DOMNodeInserted", function() {
+//   const videoCommentAuthors = document.querySelectorAll("#header-author");
+//   videoCommentAuthors.forEach(function(videoCommentAuthor) {
+//     videoCommentAuthor.insertAdjacentHTML("beforeend", '<a href="https://www.youtube.com/comments">CLICK ME</a>' )
+//   })
+// })
+
+
