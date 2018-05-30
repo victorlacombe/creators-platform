044b6ac3d3ebdf213c7bf9a3580d16d45baa1fe8
Eliott GUERIN <eliott.guerin@gmail.com>
2018-05-30 19:14:43 +0200

    div corrections

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 2018_05_30_131304) do
     t.string "channel_id_youtube"
     t.string "channel_name"
     t.string "channel_thumbnail"
-    t.string "channel_id_youtube"
     t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
