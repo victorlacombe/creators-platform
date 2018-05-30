a72d6ceac80d24591f9feb6f0ccda3f3658ed98d
Delphine Helie <heliedelphine@yahoo.fr>
2018-05-30 12:33:57 +0200

    fans card

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-10.times do |fan|
-  Fan.create([
-    {youtube_username: 'Lajika'},
-    {channel_id_youtube: 'UCfM3zsQsOnfWNUppiycmBuw'},
-    {profile_picture_url: 'https://yt3.ggpht.com/-qtnbIDAbSNQ/AAAAAAAAAAI/AAAAAAAAAAA/bb4rTjplM5Y/s288-mo-c-c0xffffffff-rj-k-no/photo.jpg'},
-    {memo_id: '1'}
-])
-end
+# 10.times do
+#     fan_attributes =[
+#     {
+#       youtube_username: 'Lajika',
+#       channel_id_youtube: 'UCfM3zsQsOnfWNUppiycmBuw',
+#       profile_picture_url: 'https://yt3.ggpht.com/a-/ACSszfFvD-sGQiDU5I60mZqxWG7s3on9iA6kjpM3tg=s176-mo-c-c0xffffffff-rj-k-no',
+#       memo_id: '1'
+#     }
+#     ]
+#   Fan.create(fan_attributes)
+# end
