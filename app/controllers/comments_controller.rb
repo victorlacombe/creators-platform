0d354ca7c1734bad92a3039252f324832c56f700
Delphine Helie <heliedelphine@yahoo.fr>
2018-05-31 13:41:18 +0200

    fanshow

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def find
+    @fan = Fan.find(params[:id])
+    comments = Comment.where(fan_id: @fan)
+    @comment = Comment.find(params[:id])
+    videos = Video.where(video_id_youtube: @comment)
+  end
+
+  private
+
+  def memo_params
+      params.require(:comment).permit(:content, :published_at, :video_id)
+  end
 end
