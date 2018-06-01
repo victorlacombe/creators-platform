6f11f174281b63ae6cd4f70dd75f2d88c5fa4d94
Victor Lacombe <37694836+victorlacombe@users.noreply.github.com>
2018-06-01 10:59:47 +0200

    Update comments_controller.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,10 +1,4 @@
 class Api::V1::CommentsController < Api::V1::BaseController
   def index
-    query = params[:query]
-    if query
-      @comments = policy_scope(Comment).where(fan_id: query)
-    else
-       @comments = policy_scope(Comment)
-    end
   end
 end
