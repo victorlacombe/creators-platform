68272f76bff25f8a1b2c20bb8153f53e6cb8cd97
Victor Lacombe <37694836+victorlacombe@users.noreply.github.com>
2018-06-01 11:00:04 +0200

    Update memos_controller.rb

--- app/controllers/api/v1/memos_controller.rb
+++ app/controllers/api/v1/memos_controller.rb
@@ -1,10 +1,4 @@
 class Api::V1::MemosController < Api::V1::BaseController
   def index
-    query = params[:query]
-    if query
-      @memos = policy_scope(Memo).where(id: query)
-    else
-      @memos = policy_scope(Memo)
-    end
   end
 end
