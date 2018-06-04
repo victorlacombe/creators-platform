fdfa5a5bbe2b0fa3f1d9f68c472ebc20a71e9cb8
Victor Lacombe <lacombe.victor@hotmail.fr>
2018-06-04 10:21:21 +0200

    deleted some useless routes

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :fans, only: [:index]
+      resources :users, only: [:index]
     end
   end
 end
