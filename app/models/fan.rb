427622227955a0bb3d9f3808f82a74cec83e96d2
Julien Chim <julien.chim@gmail.com>
2018-06-05 18:22:59 +0200

    en cours

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -18,16 +18,19 @@ class Fan < ApplicationRecord
 
   # Return an array of fans [fan, fan, fan] that has commented n_times in total
   def self.n_times_fans(user, n_times)
-    fans = user.fans.reject{ |fan| fan.channel_id_youtube == user.channel_id_youtube} if user.fans # Rejecting the current_user from results
-    fans_filtered = fans.group_by { |fan| fan.comments.count }[n_times] if fans # Hash of fans by comment count, we select fans that have commented n_times
+    # fans = user.fans.reject{ |fan| fan.channel_id_youtube == user.channel_id_youtube} if user.fans # Rejecting the current_user from results
+    fans = User.includes(fans: :comments).find(user.id).fans # Includes is to avoid N+1 requests, and instaed make a single big request. See tutorial at le Wagon.
+    rejected_user_fans = fans.reject{ |fan| fan.channel_id_youtube == user.channel_id_youtube} unless fans.empty?
+    fans_filtered = rejected_user_fans.group_by { |fan| fan.comments.size }[n_times] if rejected_user_fans # Hash of fans by comment count, we select fans that have commented n_times
     fans_filtered_recent = fans_filtered.select { |fan| fan.comments.order(published_at: :desc).first.published_at > (Date.today - 1.months) } if fans_filtered# select the fans that have recently commented
     fans_filtered_recent.nil? ? [] : fans_filtered_recent #if the result is nil, we prefer to return an empty array to avoid bug in view
   end
 
-  # Return an array of churning fans (no comments on the last 2 videos, ordered from most comments done)
+  # Return an array of churning fans (no comments the last 2 months)
   def self.churning_fans(user)
-    fans = user.fans.reject{ |fan| fan.channel_id_youtube == user.channel_id_youtube} if user.fans
-    fans_filtered = fans.group_by { |fan| fan.comments.count }.sort_by { |count, fans| -count } if fans
+    fans = User.includes(fans: :comments).find(user.id).fans
+    rejected_user_fans = fans.reject{ |fan| fan.channel_id_youtube == user.channel_id_youtube} unless fans.empty?
+    fans_filtered = rejected_user_fans.group_by { |fan| fan.comments.size }.sort_by { |count, fans| -count } if rejected_user_fans
     fans_filtered_recent = []
     if fans_filtered
       fans_filtered.each do |count, fan_arr|
@@ -39,8 +42,9 @@ class Fan < ApplicationRecord
 
   # Return an array of an array [[comment_count, fan],... ] of most loyal fans (most comments done and not churning)
   def self.top_fans(user)
-    fans = user.fans.reject{ |fan| fan.channel_id_youtube == user.channel_id_youtube} if user.fans
-    fans_filtered = fans.group_by { |fan| fan.comments.count }.sort_by { |count, fans| -count } if fans
+    fans = User.includes(fans: :comments).find(user.id).fans
+    rejected_user_fans = fans.reject{ |fan| fan.channel_id_youtube == user.channel_id_youtube} unless fans.empty?
+    fans_filtered = rejected_user_fans.group_by { |fan| fan.comments.size }.sort_by { |count, fans| -count } if fans
     fans_filtered.nil? ? [] : fans_filtered
   end
 end
